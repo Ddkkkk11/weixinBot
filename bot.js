c86cb979f6a22f3fe66289d54d7ac4fb733f6077
xdd <45672942@qq.com>
2023-05-09 12:56:13 +0800

    fix:新增axios

--- bot.js
+++ bot.js
@@ -1,13 +1,31 @@
 const { WechatyBuilder } = require('wechaty');
 const { basicService } = require("./basicService");
 const axios = require("axios");
+const { sendGpt } = require("./sendMsg");
 const wechaty = WechatyBuilder.build();
 wechaty
     .on('scan', (qrcode, status) => console.log(`Scan QR Code to login: ${status}\nhttps://wechaty.js.org/qrcode/${encodeURIComponent(qrcode)}`))
     .on('login', user => console.log(`User ${user} logged in`))
-    .on('message', (message) => {
-        if (message.text() === '西安天气' && message.mentionSelf()) {
-            message.say('西安天气还可以');
+    .on('message', async (msg) => {
+       if (msg.self()) return;
+       //消息判断
+        switch (msg.type()) {
+            case 7:
+                const room = msg.room();
+                const text = msg.text();
+                const sender = await msg.talker().alias;
+                const isMentionSelf = await msg.mentionSelf();
+                if (room && isMentionSelf) {
+                    console.log('群聊');
+                    msg.say('收到老板请稍等下');
+                    const sendText = text.replace('@铲车司机bot', '');
+                    // const res = await sendGpt(sendText);
+                    const res = await sendGpt(sendText);
+                    if (res.code === 200) {
+                        room.say(res.answer);
+                    }
+                }
+
         }
     })
 wechaty.start()
---
+++ sendMsg.js
@@ -0,0 +1,11 @@
+const {axios} = require('axios');
+const sendGpt = ( msg ) => {
+    return axios.get(`https://www.heblogs.cn/caonm/ai/o.php?msg=$(msg}&type=`)
+        .then(res => {
+            return Promise.resolve(res.data);
+        }).catch(err => {
+            return Promise.reject(err);
+        })
+
+}
+module.exports.sendGpt = sendGpt;
